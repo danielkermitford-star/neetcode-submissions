74a29d16f331d1dc81c0f3475a76e5c1f02189e7
danielkermitford-star <daniel.kermit.ford@gmail.com>
2026-09-24 10:51:28 -0700

    Bulk sync: 6 submissions

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        r = set()
+        index = {}
+        for i,n in enumerate(nums):
+            if target - n in r:
+                return [index[target - n], i]
+            r.add(n)
+            index[n] = i
